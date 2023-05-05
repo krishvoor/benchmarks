b514219222cafb96c923475dffd85b601937e651
Krishna Harsha Voora <krvoora@redhat.com>
2023-05-06 02:50:46 +0530

    Updates the following:
    1) Max CPU
    2) Min CPU
    3) MutatingAPICalls
    4) APIRequestRate
    
    Placeholder for csv writing
    
    Signed-off-by: Krishna Harsha Voora <krvoora@redhat.com>

--- hyper-scale/scripts/utils/prom.py
+++ hyper-scale/scripts/utils/prom.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
 from prometheus_api_client import PrometheusConnect
-from yaml import SafeLoader
-import time
+import csv
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 # Enhancements
@@ -8,11 +10,18 @@ import time
 # https://docs.python.org/3/library/argparse.html
 # 2) Maintain files for metric endpoint & TOKENS
 
+# Prefix o/p csv file name w/ HCP name
 # Stage MC_PROM, OBO, HC_PROM URLs
 # This can be passed as arguments to the python script
+
+# Check if mc_prom.check_prometheus_connection() is True
+# Check if hc_prom.check_prometheus_connection() is True
+# Check if obo_prom.check_prometheus_connection() is True
+
+
 mc_prom_url = "https://prometheus-k8s-openshift-monitoring.apps.hs-mc-jbahnnsi0.0j7y.s1.devshift.org"
-obo_url = "http://prometheus-hypershift-openshift-observability-operator.apps.hs-mc-jbahnnsi0.0j7y.s1.devshift.org"
-hc_url = "https://prometheus-k8s-openshift-monitoring.apps.rosa.kruiz-nug-0001.6tjk.s3.devshift.org"
+obo_prom_url = "http://prometheus-hypershift-openshift-observability-operator.apps.hs-mc-jbahnnsi0.0j7y.s1.devshift.org"
+hc_prom_url = "https://prometheus-k8s-openshift-monitoring.apps.rosa.kruiz-rrl-0001.6bzt.s3.devshift.org"
 
 # Stage MC_BEARER, OBO_BEARER, HC_BEARER
 mc_prom_header = {"Authorization": "Bearer <UPDATE_ME>"}
@@ -24,37 +33,89 @@ hc_header = {"Authorization": "Bearer <UPDATE_ME>"}
 mc_prom = PrometheusConnect(url=mc_prom_url, headers=mc_prom_header, disable_ssl=True)
 
 # Connect to OBO
-obo_prom = PrometheusConnect(url=obo_url, headers=obo_header, disable_ssl=False)
+obo_prom = PrometheusConnect(url=obo_prom_url, headers=obo_header, disable_ssl=False)
 
 # Connect to HC_PROM
-hc_prom = PrometheusConnect(url=hc_url, headers=hc_header, disable_ssl=False)
+hc_prom = PrometheusConnect(url=hc_prom_url, headers=hc_header, disable_ssl=False)
+
+
+# Variables to be used
+HCP_NAMESPACE = "ocm-staging-23fd33s6u5dr56rbvivaciovsthu9qdm-kruiz-rrl-0001"
+DEPLOYMENT_NAME = "openshift-apiserver"
+CONTAINER_NAME = "kube-apiserver"
 
 
 ################################################################################################
-# MC Metrics from MC_Prom
+# MC Metrics from HC_Prom
 ################################################################################################
-# Check if mc_prom.check_prometheus_connection() is True
 
-mc_prom_kube_node_role = ' sum by (node) ( kube_node_role{role=~"master|infra|workload|obo" } ) '
-mc_data_kube_node_role = mc_prom.get_current_metric_value(mc_prom_kube_node_role)
-print(mc_data_kube_node_role)
+# MaxMemory
+mc_max_mem_container = 'max(max_over_time(container_memory_working_set_bytes{pod=~"kube-apiserver.*", namespace="ocm-staging-23fd33s6u5dr56rbvivaciovsthu9qdm-kruiz-rrl-0001"}[2m]))'
+mc_max_mem_container_data = mc_prom.custom_query(mc_max_mem_container)
+print(mc_max_mem_container_data)
 
+# MinMemory
+mc_min_mem_container = 'min(max_over_time(container_memory_working_set_bytes{pod=~"kube-apiserver.*", namespace="ocm-staging-23fd33s6u5dr56rbvivaciovsthu9qdm-kruiz-rrl-0001"}[2m]))'
+mc_min_mem_container_data = mc_prom.custom_query(mc_min_mem_container)
+print(mc_min_mem_container_data)
+
+
+# P95 CPU
+mc_95_cpu_container = 'histogram_quantile(0.95, rate(kube_pod_container_resource_requests{namespace=~"ocm-staging-23fd33s6u5dr56rbvivaciovsthu9qdm-kruiz-rrl-0001", resource="cpu", unit="core", pod=~"kube-apiserver-.*"}[30m]))'
+mc_95_cpu_container_data = mc_prom.custom_query(mc_95_cpu_container)
+
+# P90 CPU
+mc_90_cpu_container = 'histogram_quantile(0.90, rate(kube_pod_container_resource_requests{namespace=~"ocm-staging-23fd33s6u5dr56rbvivaciovsthu9qdm-kruiz-rrl-0001", resource="cpu", unit="core", pod=~"kube-apiserver-.*"}[30m]))'
+mc_90_cpu_container_data = mc_prom.custom_query(mc_90_cpu_container)
+
+# P75 CPU
+mc_75_cpu_container = 'histogram_quantile(0.75, rate(kube_pod_container_resource_requests{namespace=~"ocm-staging-23fd33s6u5dr56rbvivaciovsthu9qdm-kruiz-rrl-0001", resource="cpu", unit="core", pod=~"kube-apiserver-.*"}[30m]))'
+mc_75_cpu_container_data = mc_prom.custom_query(mc_75_cpu_container)
 
 ################################################################################################
 # HCP Metrics from HC_Prom
 ################################################################################################
-# Check if hc_prom.check_prometheus_connection() is True
 
-hc_query_containerCPU_Workers = '(avg(irate(container_cpu_usage_seconds_total{name!="",container!="POD",namespace=~"openshift-(sdn|ovn-kubernetes|ingress)"}[2m]) * 100 and on (node) kube_node_role{role="worker"}) by (namespace, pod, container)) > 0' 
-hc_data_containerCPU_Workers= hc_prom.custom_query(hc_query_containerCPU_Workers)
-print(hc_data_containerCPU_Workers)
+
+
+#hc_query_containerCPU_Workers = '(avg(irate(container_cpu_usage_seconds_total{name!="",container!="POD",namespace=~"openshift-(sdn|ovn-kubernetes|ingress)"}[2m]) * 100 and on (node) kube_node_role{role="worker"}) by (namespace, pod, container)) > 0' 
+#hc_data_containerCPU_Workers= hc_prom.custom_query(hc_query_containerCPU_Workers)
+#print(hc_data_containerCPU_Workers)
+
 
 
 ################################################################################################
 # OBO Metrics from OBO_Prom
 ################################################################################################
-# Check if obo_prom.check_prometheus_connection() is True
 
-obo_query_APIRequestRate = '( sum( irate( apiserver_request_total{namespace="ocm-staging-23c1ih4sou995ndcavpgfkbej9dr9r8f-xuelihp04274", job="apiserver", verb!="WATCH"} [2m] ) ) by (verb,resource,instance) ) > 0' 
-obo_data_APIRequestRate = obo_prom.custom_query(obo_query_APIRequestRate)
-print(obo_data_APIRequestRate)
+# Needs formatting
+obo_schedulingThroughput = 'irate(apiserver_request_total {{namespace=~"{}", verb="POST", resource="pods", subresource="binding", code="201"}}[2m]) > 0'.format(HCP_NAMESPACE)
+obo_schedulingThroughput_data = obo_prom.custom_query(obo_schedulingThroughput)
+print(obo_schedulingThroughput_data)
+
+
+obo_APIRequestRate = ' sum ( irate ( apiserver_request_total {namespace=~"HCP_NAMESPACE}", job="kube-apiserver",verb!="WATCH"}[2m]) ) by ( verb, resource, instance) > 0 '
+obo_APIRequestRate_data = obo_prom.custom_query(obo_APIRequestRate)
+print(obo_APIRequestRate_data)
+
+obo_mutatingAPICallsLatency = ' histogram_quantile (0.99, sum ( irate ( apiserver_request_duration_seconds_bucket{namespace=~"HCP_NAMESPACE", job="kube-apiserver", verb=~"POST|PUT|DELETE|PATCH", subresource!~"log|exec|portforward|attach|proxy"}[2m])) by ( le, resource, verb, scope)) > 0'
+obo_mutatingAPICallsLatency_data = obo_prom.custom_query(obo_mutatingAPICallsLatency)
+print(obo_mutatingAPICallsLatency_data)
+
+
+
+# Define the path to the CSV file
+# To-do:
+# Append the HCP Name, iteration name
+output_csv_file = 'final_output.csv'
+
+
+# Save the results into a CSV file
+with open('output_csv_file', 'w', newline='') as csvfile:
+    # Move to end of the file
+    csvfile.seek(0, 2)
+    print(" Moved to end of file")
+    # Dump the data now
+    # Reset cursor to file starting and close the file
+    csvfile.seek(0)
+    csvfile.close()
